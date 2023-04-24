ce40f0864685f9d097a2a58cc318030da7128c22
Martin ‘Kvík’ Baláž <martin.balaz@trojsten.sk>
2023-04-24 17:30:48 +0000

    Removed ZernikeDiscrete

--- correctors/__init__.py
+++ correctors/__init__.py
@@ -1,4 +1,3 @@
 from .interpolator import Interpolator
 from .zernike import ZernikeExpander, ZernikeFitter
-from .zernikediscrete import ZernikeDiscreteExpander
 from .kernelsmoother import KernelSmoother
