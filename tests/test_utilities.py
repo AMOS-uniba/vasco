b1279321d45bcb5f3b643286396ed985f197a083
Martin Baláž <martin.balaz@trojsten.sk>
2026-05-27 17:36:02 +0000

    Removed tests from demeteor

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from vasco.utilities import disk_to_altaz, altaz_to_disk, spherical_distance
+from vasco.utilities import disk_to_altaz, altaz_to_disk
 from .base import pytest_generate_tests
 
 
@@ -19,35 +19,3 @@ class TestAngularFunction:
         point = np.array([[x, y]])
         assert np.allclose(altaz_to_disk(disk_to_altaz(point)), point)
 
-
-class TestSpherical:
-    def test_distance(self):
-        dist = spherical_distance(np.array([0, 0]), np.array([0, np.pi / 2]))
-        assert np.allclose(dist, np.pi / 2)
-
-    def test_distance_2(self):
-        assert np.allclose(spherical_distance(np.array([0, 1]), np.array([0, 1.1])), 0.1)
-
-    def test_distance_3(self):
-        assert np.allclose(
-            spherical_distance(
-                np.array([np.radians(45), np.radians(45)]),
-                np.array([np.radians(37), np.radians(-115)])
-            ),
-            1.67610715)
-
-    def test_distance_4(self):
-        assert np.allclose(
-            spherical_distance(
-                np.array([np.radians(22), np.radians(98)]),
-                np.array([np.radians(-36), np.radians(14)])
-            ),
-            1.71305632)
-
-    def test_distance_5(self):
-        assert np.allclose(
-            spherical_distance(
-                np.array([np.radians(22), np.radians(98)]),
-                np.array([np.radians(-22), np.radians(-82)])
-            ),
-            np.pi)
